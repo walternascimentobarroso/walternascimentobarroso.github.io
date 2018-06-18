f0fd8e0e06feb7949943f3192764de00e139cadd
Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>
2018-06-18 16:26:42 -0400

    [FIX] Fix the travis in branch development
    Signed-off-by: Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e # termina o script com um código diferente de 0 se alguma coisa falhar
+
+# instal all dependency
+npm install
+
+# gerando arquivos para deploy
+gulp deploy
+
+# entre na pasta onde está o build do seu projeto e inicie um novo repositório git
+cd dist
+git init
+
+# dentro desse repositório nós pretendemos ser um novo usuário
+git config user.name "Travis CI"
+git config user.email "walter.nascimento.barroso@gmail.com"
+
+# O primeiro e único commit do seu repositório terá
+# todos os arquivos presentes e a mensagem do commit será "Deploy to GitHub Pages"
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+# Forçando o push (Toda história anterior da branch master
+# será perdido, pois vamos substituí-lo.)  Redirecionamos qualquer saída para
+# /dev/null para ocultar quaisquer dados de credenciais sensíveis que de outra forma possam ser expostos.
+# tokens GH_TOKEN e GH_REF serão fornecidos como variáveis de ambiente Travis CI
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
+
+# exit the script
+exit 0
