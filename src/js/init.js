d9b12326dbc6595b57b651312326769e30a07789
Walter Nascimento Barroso <walternascimentobarroso@gmail.com>
2020-03-27 12:41:35 -0400

    :sparkles: [ADD] Add theme for homepage

--- src/js/init.js
+++ src/js/init.js
@@ -15,4 +15,32 @@ document.addEventListener("DOMContentLoaded", function(event) {
     if(isMobile()) {
     } else {
     }
+
+
+    function toggleTheme () {
+        const htmlTag = document.getElementsByTagName('html')[0]
+        if (htmlTag.hasAttribute('data-theme')) {
+            htmlTag.removeAttribute('data-theme')
+            return window.localStorage.removeItem("site-theme")
+        }
+    
+        htmlTag.setAttribute('data-theme', 'dark')
+        window.localStorage.setItem("site-theme", "dark")
+    }
+    
+    function applyInitialTheme () {
+        const theme = window.localStorage.getItem("site-theme")
+        if (theme !== null) {
+            const htmlTag = document.getElementsByTagName("html")[0]
+            htmlTag.setAttribute("data-theme", theme)
+        }
+    }
+    
+    applyInitialTheme();
+    
+    document
+        .getElementById("theme-toggle")
+        .addEventListener("click", toggleTheme);
+    
+
 });
