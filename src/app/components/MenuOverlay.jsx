c390900cd41681f1aeb15f1f1c3dddf16012a69b
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-12-14 13:00:42 +0000

    :sparkles: Add new component - Navbar, Navlink, MenuOverlay
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

---
+++ src/app/components/MenuOverlay.jsx
@@ -0,0 +1,13 @@
+import {NavLink} from "./NavLink";
+
+export const MenuOverlay = ({ links }) => {
+  return (
+    <ul className="flex flex-col py-4 items-center">
+      {links.map((link, index) => (
+        <li key={index}>
+          <NavLink href={link.path} title={link.title} />
+        </li>
+      ))}
+    </ul>
+  );
+};
