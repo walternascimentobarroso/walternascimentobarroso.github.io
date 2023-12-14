172358fc0a30d9b52a8220c35cd4375726203f0e
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-12-14 13:27:06 +0000

    :sparkles: Add new component - AboutSection, TabButton
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import { HeroSection } from "./components/HeroSection";
 import { Navbar } from "./components/Navbar";
+import { HeroSection } from "./components/HeroSection";
+import { AboutSection } from "./components/AboutSection";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Navbar />
       <div className="container mx-auto px-12 py-4">
         <HeroSection />
+        <AboutSection />
       </div>
     </main>
   );
