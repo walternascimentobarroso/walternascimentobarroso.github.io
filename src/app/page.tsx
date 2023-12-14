27d7a9c07e59775434e6c94f820bb669b949d5f1
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-12-14 13:59:07 +0000

    :sparkles: Add new component - ProjectCard, ProjectsSection, ProjectTag
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "./components/Navbar";
 import { HeroSection } from "./components/HeroSection";
 import { AboutSection } from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="container mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
+        <ProjectsSection />
       </div>
     </main>
   );
