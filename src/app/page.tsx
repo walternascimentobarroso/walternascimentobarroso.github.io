29987cf48ddb7c5e502e5ef94f959862b17f87aa
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-12-14 12:33:14 +0000

    :sparkles: Add new component - HeroSection
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { HeroSection } from "./components/HeroSection";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Portfolio Website</h1>
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <div className="container mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }
