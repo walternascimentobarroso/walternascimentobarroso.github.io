51d3ddf28e3ba2c99bc89e2def82062632c7a03b
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-12-15 15:42:13 +0000

    :bug: Fix not env variables
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+const resend = new Resend(process.env.RESEND_API_KEY ?? "none");
+const fromEmail = process.env.FROM_EMAIL ?? "none@mail.com";
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
