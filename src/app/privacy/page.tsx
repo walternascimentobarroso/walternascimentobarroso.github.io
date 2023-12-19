93922034a63113d6c810b2513ab9bda4718e4bea
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-12-19 12:53:03 +0000

    :lock: Add privacy
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,83 @@
+export default function PrivacyPolicies() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <h2>Política de Privacidade para a Extensão Gruppy</h2>
+        <p>
+          Esta Política de Privacidade descreve como as informações são
+          coletadas, usadas e compartilhadas pela extensão Gruppy para Chrome.
+        </p>
+
+        <h2>Informações Coletadas</h2>
+
+        <p>A extensão Gruppy pode coletar as seguintes informações:</p>
+
+        <p>
+          <span> 1. Informações Fornecidas pelos Usuários: </span>
+          <span>
+            - A extensão pode coletar informações que você fornece
+            voluntariamente, como tokens e parâmetros de loja.
+          </span>
+          <span>2. Informações Automáticas: </span>
+          <span>
+            - A extensão pode coletar automaticamente informações sobre o seu
+            uso, como logs e estatísticas para melhorar a funcionalidade.
+          </span>
+        </p>
+
+        <h2>Uso das Informações</h2>
+
+        <p>
+          <span> As informações coletadas podem ser utilizadas para:</span>
+          <span>
+            1. Abrir janelas ou abas no navegador de acordo com as preferências
+            do usuário.
+          </span>
+          <span>2. Aprimorar a funcionalidade da extensão. </span>
+          <span>3. Personalizar a experiência do usuário.</span>
+        </p>
+
+        <h2>Proteção e Segurança</h2>
+
+        <p>
+          Tomamos medidas para proteger as informações dos usuários, incluindo a
+          implementação de práticas de segurança e a utilização de criptografia.
+        </p>
+
+        <h2>Compartilhamento de Informações</h2>
+
+        <p>
+          A extensão Gruppy não compartilha informações pessoais com terceiros,
+          exceto quando necessário para fornecer a funcionalidade da extensão.
+        </p>
+
+        <h2>Cookies e Tecnologias Semelhantes</h2>
+
+        <p>
+          A extensão Gruppy pode usar cookies ou tecnologias semelhantes para
+          melhorar a experiência do usuário. Os usuários podem gerenciar
+          configurações relacionadas a essas tecnologias.
+        </p>
+
+        <h2>Alterações na Política de Privacidade</h2>
+
+        <p>
+          Reservamo-nos o direito de fazer alterações nesta Política de
+          Privacidade. Os usuários serão notificados sobre alterações
+          significativas.
+        </p>
+
+        <h2>Informações de Contato</h2>
+
+        <p>
+          Se você tiver dúvidas ou preocupações sobre esta Política de
+          Privacidade, entre em contato conosco em walter.nascimento@gruppy.com.br.
+        </p>
+
+        <h2>Data de Vigência</h2>
+
+        <p>Esta Política de Privacidade é efetiva a partir de 18/12/2023.</p>
+      </div>
+    </main>
+  );
+}
